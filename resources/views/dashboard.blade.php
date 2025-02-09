c65977d8535f5506f3dbca43ce12792319fc87f0
Volters <kevinsvolters12@gmail.com>
2025-02-10 01:35:59 +0200

    Added buttons and nav

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,8 +6,26 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @include('layouts.navigation')
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <div class="flex flex-col space-y-4 text-center">
+                        <a href="/"
+                            class="px-6 py-3 bg-yellow-400 text-gray-900 font-bold rounded-lg hover:bg-yellow-500 transition">
+                            Start game
+                        </a>
+
+                        <a href="/"
+                            class="px-6 py-3 bg-gray-800 text-white font-bold rounded-lg hover:bg-gray-700 transition">
+                            How to Play
+                        </a>
+
+                        <a href="/"
+                            class="px-6 py-3 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600 transition">
+                            Settings
+                        </a>
+                    </div>
+
+
                 </div>
             </div>
         </div>
