bf977fdeb21bd26626f285cc90db02c7125046ff
Volters <kevinsvolters12@gmail.com>
2025-02-10 00:29:44 +0200

    / takes you to auth

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
