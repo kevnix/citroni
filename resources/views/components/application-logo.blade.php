e3bc7481076e26564bc06793c5ab8bc7e5d72f8a
Volters <kevinsvolters12@gmail.com>
2025-02-10 03:51:12 +0200

    Fixed header logo size

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/logo_transparent.png') }}" alt="Logo" >
+<img src="{{ asset('images/logo_transparent.png') }}" alt="Logo" class="h-64 w-auto object-scale-down" >
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
             <!-- Page Heading -->
             @isset($header)
                 <header class="bg-transparent">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-center">
+                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center">
                         {{ $header }}
                     </div>
                 </header>
