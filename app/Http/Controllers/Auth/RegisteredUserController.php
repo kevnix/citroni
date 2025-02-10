1613b399501221cfe88909f68e2226a50e7f6041
Volters <kevinsvolters12@gmail.com>
2025-02-10 03:20:55 +0200

    Give new player 1450 balance

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,6 +38,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'balance' => 1450,
             'password' => Hash::make($request->password),
         ]);
 
